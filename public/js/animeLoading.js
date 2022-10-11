8deaa2fbdf1659fc01091300d8249dbf2caf4f37
Marthieu2 <matthieu.evuort@efrei.net>
2022-10-11 13:12:44 -0700

    Update of the website : if you click on the button and then you push again on it, the text disappears to show the background image

--- public/js/animeLoading.js
+++ public/js/animeLoading.js
@@ -1,6 +1,11 @@
 // Get page elements
 var Anime1 = document.getElementById("Anime1");
 var Anime2 = document.getElementById("Anime2");
+var Anime3 = document.getElementById("Anime3");
+var Anime4 = document.getElementById("Anime4");
+var Anime5 = document.getElementById("Anime5");
+var Anime6 = document.getElementById("Anime6");
+var Anime7 = document.getElementById("Anime7");
 
 var containerAnime = document.getElementById("containerAnime");
 var titleAnime = document.getElementsByClassName("titleAnime")[0];
@@ -9,6 +14,13 @@ var synopsisAnime = document.getElementById("synopsisAnime");
 
 Anime1.addEventListener("click", () => animeGet(1));
 Anime2.addEventListener("click", () => animeGet(2));
+Anime3.addEventListener("click", () => animeGet(3));
+Anime4.addEventListener("click", () => animeGet(4));
+Anime5.addEventListener("click", () => animeGet(5));
+Anime6.addEventListener("click", () => animeGet(6));
+Anime7.addEventListener("click", () => animeGet(7));
+
+
 
 function animeGet(i) {
   fetch(`/api/animeGet/${i}`)
@@ -27,14 +39,38 @@ function animeGet(i) {
       changeImage(i);
     });
 
-    containerAnime.style.display = "block";
+    if(getComputedStyle(containerAnime).display != "none"){
+      containerAnime.style.display = "none";
+    } else {
+      containerAnime.style.display = "block";
+    }
 }
 
+
+
 function changeImage(i){
-  if(i==1){
-    document.body.style.backgroundImage = "url('/image/background1.webp')";
-  }
-  else{
-    document.body.style.backgroundImage = "url('/image/background.webp')";
+  switch(i){
+    case 1:
+      return document.body.style.backgroundImage = "url('/image/background1.webp')";
+      break;
+    case 2:
+      return document.body.style.backgroundImage = "url('/image/background.webp')";
+      break;
+    case 3:
+      return document.body.style.backgroundImage = "url('/image/background3.webp')";
+        break;
+     case 4:
+        return document.body.style.backgroundImage = "url('/image/background4.webp')";
+         break;
+      case 5:
+        return document.body.style.backgroundImage = "url('/image/background5.webp')";
+         break;
+      case 6:
+        return document.body.style.backgroundImage = "url('/image/background6.webp')";
+        break;
+      case 7:
+        return document.body.style.backgroundImage = "url('/image/background7.webp')";
+        break;
+    
   }
 }
